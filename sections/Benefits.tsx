8ec2eaeef0154b0039617297029ba530839efd46
Jaqueline Silva <jscnewlf@gmail.com>
2023-05-21 11:54:34 -0300

    Update Benefits.tsx

--- sections/Benefits.tsx
+++ sections/Benefits.tsx
@@ -1,55 +1,45 @@
 export interface Props {
-    benefits?: Array<{
-      srcImg: string;
-      benefitsTitle: string;
-      benefitsDesc: string;
-    }>;
-    benefitsWrapper?: {
-      benefitTitle: string;
-      benefitDescription: string;
-    };
-  }
-  
-  export default function Benefits({ benefits, benefitsWrapper }: Props) {
-    return (
-      <div className="flex flex-col items-center justify-center p-4">
-        {benefitsWrapper
-          ? (
-            <>
-              <div className="text-3xl w-fit text-[#292929]">
-                {benefitsWrapper.benefitTitle}
-              </div>
-              <div className="text-lg w-fit my-4 text-neutral-500">
-                {benefitsWrapper.benefitDescription}
-              </div>
-            </>
-          )
-          : <div>adiciona no library</div>}
-        <div className="flex flex-col gap-2 w-[100%] md:flex-row">
-          {benefits === undefined
-            ? <div>adiciona algo no library</div>
-            : (benefits.map((benefit, idx) => {
-              if (idx !== benefits.length - 1) {
-                return (
-                  <>
-                    <div className="flex flex-row items-center gap-4 mx-auto w-[80%]">
-                      <div>
-                        <img src={benefit.srcImg} width={25} height={25} alt="benefit img" />
-                      </div>
-                      <div className="content">
-                        <p className="text-[#292929]">{benefit.benefitsTitle}</p>
-                        <span className="text-neutral-500">
-                          {benefit.benefitsDesc}
-                        </span>
-                      </div>
-                    </div>
-                    <div className="divider md:divider-horizontal"></div>
-                  </>
-                );
-              } else {return (
+  benefits?: Array<{
+    srcImg: string;
+    benefitsTitle: string;
+    benefitsDesc: string;
+  }>;
+  benefitsWrapper?: {
+    benefitTitle: string;
+    benefitDescription: string;
+  };
+}
+
+export default function Benefits({ benefits, benefitsWrapper }: Props) {
+  return (
+    <div className="flex flex-col items-center justify-center p-4">
+      {benefitsWrapper
+        ? (
+          <>
+            <div className="text-3xl w-fit text-[#292929]">
+              {benefitsWrapper.benefitTitle}
+            </div>
+            <div className="text-lg w-fit my-4 text-neutral-500">
+              {benefitsWrapper.benefitDescription}
+            </div>
+          </>
+        )
+        : <div>adiciona no library</div>}
+      <div className="flex flex-col gap-2 w-[100%] md:flex-row">
+        {benefits === undefined
+          ? <div>adiciona algo no library</div>
+          : (benefits.map((benefit, idx) => {
+            if (idx !== benefits.length - 1) {
+              return (
+                <>
                   <div className="flex flex-row items-center gap-4 mx-auto w-[80%]">
                     <div>
-                      <img src={benefit.srcImg}  width={25} height={25} alt="benefit img" />
+                      <img
+                        src={benefit.srcImg}
+                        width={25}
+                        height={25}
+                        alt="benefit img"
+                      />
                     </div>
                     <div className="content">
                       <p className="text-[#292929]">{benefit.benefitsTitle}</p>
@@ -58,11 +48,31 @@ export interface Props {
                       </span>
                     </div>
                   </div>
-                );}
-            }))}
-  
-          {/*  */}
-        </div>
+                  <div className="divider md:divider-horizontal"></div>
+                </>
+              );
+            } else {return (
+                <div className="flex flex-row items-center gap-4 mx-auto w-[80%]">
+                  <div>
+                    <img
+                      src={benefit.srcImg}
+                      width={25}
+                      height={25}
+                      alt="benefit img"
+                    />
+                  </div>
+                  <div className="content">
+                    <p className="text-[#292929]">{benefit.benefitsTitle}</p>
+                    <span className="text-neutral-500">
+                      {benefit.benefitsDesc}
+                    </span>
+                  </div>
+                </div>
+              );}
+          }))}
+
+        {/*  */}
       </div>
-    );
-  }
+    </div>
+  );
+}
